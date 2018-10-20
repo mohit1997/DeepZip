22ba12021fb5f78f56a1de89b0aa6f22f9c2392b
mohit1997 <goyal.mohit999@gmail.com>
2018-10-21 01:39:55 +0530

    upload extra codes to geenerate results

---
+++ to_be_run/generate_markov.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def generate_markov(seq_length):
+	a = np.random.choice(2, 100)
+	l = list(a)
+	for _ in range(seq_length-100):
+		l.append(np.logical_xor(l[-5], l[-1]))
+
+	a = np.array(l)
+	np.save('markov_seq', a)
+
+if __name__ == "__main__":
+	generate_markov(1000000)
