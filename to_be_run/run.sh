00630e81442cbc40d14b8f0405161c27667a0452
Kedar Tatwawadi <ktatwawadi@gmail.com>
2018-10-24 09:25:21 -0700

    adding run.sh

--- to_be_run/run.sh
+++ to_be_run/run.sh
@@ -1,14 +1,19 @@
 #/bin/bash
-model_dir=""
-data_dir=""
-scripts_dir=""
+model_dir="models_$1"
+data_dir="../Parser/chromosomes_N"
+export CUDA_VISIBLE_DEVICES=$2
 
-for f in $data_dir/*.npy do
+for f in $data_dir/*.npy 
+do
     echo $f
     s=${f##*/}
     basename=${s%.*}
     echo $basename
     model_name="$model_dir/$basename.hdf5"
+    echo $model_name
+    script_name=$1.py
+    echo $script_name
+    echo "- - - - - "
 done
 
 
